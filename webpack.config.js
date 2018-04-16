fb26bb42f8a62263f0cbfecaa04f77d8a0032b02
Phil Price <pprice@microsoft.com>
2018-04-16 16:46:09 -0700

    Fix sourcemaps for vscode

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         // necessary for HMR to know where to load the hot update chunks
     },
 
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
 
     module: {
         rules: [
